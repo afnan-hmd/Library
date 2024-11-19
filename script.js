5dcbb053454e558a2a0124be6868133b6188de24
afnan-hmd <91302955+afnan-hmd@users.noreply.github.com>
2024-11-19 11:02:18 +0400

    Edit card css

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, title, pages, completed) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.completed = completed;
+    this.starred = starred;
+    this.bookmarked = bookmarked;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function removeBook() {
+
+}
