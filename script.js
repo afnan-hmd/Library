e0bdc851d0eafe28420a43f2043e3611ef0fb7be
afnan-hmd <91302955+afnan-hmd@users.noreply.github.com>
2024-11-24 10:47:47 +0400

    Edit style

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const addBookBtn = document.querySelector('#add')
+
+
 const myLibrary = [];
 
-function Book(title, title, pages, completed) {
+function Book(title, author, pages, completed, starred, bookmarked) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    
     this.completed = completed;
     this.starred = starred;
     this.bookmarked = bookmarked;
@@ -16,3 +20,14 @@ function addBookToLibrary() {
 function removeBook() {
 
 }
+
+
+
+addBookBtn.addEventListener('click', function(e) {
+
+});
+// when page loads: display library list content
+// when add book button is clicked
+//      create a new book object
+//      add new object to library list
+//      display updated list content as cards 
